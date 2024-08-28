b87dd0c8a14d0ecec01af7400f04b537a9973217
keemzleun <92363679+keemzleun@users.noreply.github.com>
2024-08-28 11:49:23 +0900

    fix

--- src/main/java/beyond/ordersystem/common/configs/CorsConfig.java
+++ src/main/java/beyond/ordersystem/common/configs/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry){
         corsRegistry.addMapping("/**")
-                .allowedOrigins("http://www.keemzleun.shop")    // 허용 url 명시
+                .allowedOrigins("https://www.keemzleun.shop")    // 허용 url 명시
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
